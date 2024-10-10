54c6e353e839d2300a43c8919656c9f4349906d3
AbdulMoyeen <moyeenkhan47@gmail.com>
2024-10-10 20:43:06 +0530

    second commit

--- User-Service/User-Service/src/main/java/com/user/service/Impl/UserServiceImpl.java
+++ User-Service/User-Service/src/main/java/com/user/service/Impl/UserServiceImpl.java
@@ -13,6 +13,9 @@ import com.user.model.OrderReport;
 import com.user.repository.UserRepository;
 import com.user.service.UserService;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -20,6 +23,9 @@ public class UserServiceImpl implements UserService {
 	private UserRepository userRepository;
 	@Autowired
 	private FeignClientCall feignClientCall;
+	
+	@Autowired
+	private CircuitBreakerRegistry breakerRegistry;
 
 	@Override
 	public User createUser(User user) {
@@ -42,11 +48,24 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
+	@CircuitBreaker(name = "userService" , fallbackMethod = "fallBackMethod")
 	public OrderReport order(OrderReport orderReport) {
 		Optional<User> user = userRepository.findById(orderReport.getUserId());
 		orderReport.setEmail(user.get().getEmail());
 		orderReport.setFullName(user.get().getFullName());
 		return feignClientCall.order(orderReport);
+	}
+	
+	public OrderReport fallBackMethod(OrderReport orderReport,Throwable throwable) {
+		System.out.println("Circuite breaker activated. Error:"+throwable.getMessage());
+		io.github.resilience4j.circuitbreaker.CircuitBreaker cb = breakerRegistry.circuitBreaker("userService");
+		String state = cb.getState().toString();
+
+		OrderReport fallBackOrder=new OrderReport();
+		fallBackOrder.setUserId(orderReport.getUserId());
+		fallBackOrder.setFullName(state);
+		fallBackOrder.setEmail(state);
+		return fallBackOrder;
 	}
 
 }
